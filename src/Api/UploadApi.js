0892b0a63c47ea343ac47eb7215002b729c7fb31
fariborz shalghooni <fariborz0015@gmail.com>
2021-04-18 15:40:54 +0430

    fix timeout of uplaod profile

--- src/Api/UploadApi.js
+++ src/Api/UploadApi.js
@@ -7,7 +7,7 @@ let api_token=window.localStorage.getItem("api_token");
 const instance = axios.create({
 
     baseURL:'http://laravelapi.dct-roosh-hirkan.ir/api',
-    timeout: 5000,
+    timeout: 20000,
     headers:{
         Authorization:`Bearer ${api_token}` ,
         Accept: 'application/json',
